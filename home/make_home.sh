ad4b96a57b4d9044b3a7370969dc1519e7cf1fdf
Xiaoran Shen <xs4587@my.utexas.edu>
2026-09-03 00:08:29 +0800

    Add home creation script.

---
+++ home/make_home.sh
@@ -0,0 +1,15 @@
+mkdir -p home/bedroom
+mkdir -p home/kitchen
+mkdir -p home/living_room
+
+touch home/bedroom/bed.txt
+touch home/bedroom/desk.txt
+touch home/bedroom/lamp.txt
+
+touch home/kitchen/refrigerator.txt
+touch home/kitchen/stove.txt
+touch home/kitchen/table.txt
+
+touch home/living_room/sofa.txt
+touch home/living_room/tv.txt
+touch home/living_room/bookshelf.txt
